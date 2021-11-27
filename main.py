1a9d486fd51351f227b8a51fecf1b7010773d980
demitriy <dima.golovanov.2005@mail.ru>
2021-11-27 13:42:04 +0300

    comm2

--- main.py
+++ main.py
@@ -6,6 +6,49 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 
 
+class Yes(QMainWindow):
+    def __init__(self, main):
+        self.main = main
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.setFixedSize(self.size().width(), self.size().height())
+        self.connection = sqlite3.connect("coffee.db")
+        self.lines = [self.lineEdit, self.lineEdit_2,self.lineEdit_3,
+                  self.lineEdit_4,self.lineEdit_5,self.lineEdit_6]
+        self.flag = False
+        self.pushButton.clicked.connect(self.p)
+
+    def add(self):
+        self.flag = False
+        for e in self.lines:
+            e.setText("")
+        self.show()
+
+    def edit(self, id, six):
+        self.flag = True
+        self.id = id
+        for e in range(6):
+            self.lines[e].setText(six[e])
+        self.show()
+
+    def p(self):
+        if self.flag:
+            self.connection.cursor().execute("""UPDATE sorts
+                                SET title = ?, power = ?, ground = ?, taste = ?, price = ?, volume = ?
+                                WHERE id = ?""", tuple(map(lambda x: x.text(), self.lines)) + tuple([self.id]))
+            self.connection.commit()
+            self.hide()
+            self.main.loadTable()
+            return
+        else:
+            self.connection.cursor().execute("""INSERT INTO sorts(title, power, ground, taste, price, volume)
+                                 VALUES(?, ?, ?, ?, ?, ?)""", tuple(map(lambda x: x.text(), self.lines)))
+            self.connection.commit()
+            self.hide()
+            self.main.loadTable()
+            return
+
+
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -13,6 +56,9 @@ class MyWidget(QMainWindow):
         self.setFixedSize(self.size().width(), self.size().height())
         self.sqlconnect = sqlite3.connect("coffee.db")
         self.loadTable()
+        self.pushButton.clicked.connect(self.add)
+        self.pushButton_2.clicked.connect(self.edit)
+        self.yes = Yes(self)
 
     def loadTable(self):
         data = self.sqlconnect.cursor().execute("""SELECT * FROM sorts""")
@@ -27,6 +73,17 @@ class MyWidget(QMainWindow):
                 self.tableWidget.setItem(
                     i, j, QTableWidgetItem(str(elem)))
         self.tableWidget.resizeColumnsToContents()
+
+    def add(self):
+        self.yes.add()
+
+    def edit(self):
+        ind = self.tableWidget.currentRow()
+        a = []
+        for e in range(1, 7):
+            a.append(self.tableWidget.item(ind, e).text())
+        ind = int(self.tableWidget.item(ind, 0).text())
+        self.yes.edit(ind, a)
 
 
 if __name__ == '__main__':
